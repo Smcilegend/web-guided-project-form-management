64b8906b114c33a4181333eb6bca6975d75f26d3
Michael Gibson <selfmadecultureinc@gmail.com>
2025-01-08 11:29:14 -0600

    Guided_Project_Forms

--- src/components/App.js
+++ src/components/App.js
@@ -16,19 +16,35 @@ export default function App() {
   const [friends, setFriends] = useState([]) // careful what you initialize your state to
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const formValues = {} // fix this using the state hook
+  const [formValues, setFormValues] = useState(initialFormValues) // fix this using the state hook
 
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
-    //  It takes in the name of an input and its value, and updates `formValues`
+    // It takes in the name of an input and its value, and updates `formValues`
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue,
+    })
   }
 
   const submitForm = () => {
     // 🔥 STEP 9 - IMPLEMENT a submit function which will be used inside the form's own `onSubmit`
-    //  a) make a new friend object, trimming whitespace from username and email
-    //  b) prevent further action if either username or email or role is empty string after trimming
-    //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
-    //  d) also on success clear the form
+    // a) make a new friend object, trimming whitespace from username and email
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    // b) prevent further action if either username or email or role is empty string after trimming
+    if (!newFriend.username || !newFriend.email || !newFriend.role) return
+    // c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
+    axios.post('fakeapi.com', newFriend)
+      .then(res => {
+        const friendFromBackend = res.data // includes a unique 'id'
+        setFriends([friendFromBackend, ...friends])
+        // d) also on success clear the form
+        setFormValues(initialFormValues)
+      })
   }
 
   useEffect(() => {
@@ -41,9 +57,11 @@ export default function App() {
 
       <FriendForm
         // 🔥 STEP 2 - The form component needs its props.
-        //  Check implementation of FriendForm
-        //  to see what props it expects.
-        values={{}}
+        //    Check implementation of FriendForm
+        //    to see what props it expects.
+        update={updateForm}
+        submit={submitForm}
+        values={formValues}
       />
 
       {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,69 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React, { useState } from "react";
+import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 First let's build a SimpleForm to add more pets:
 const petsList = [
-  { petName: 'Fido', petType: 'dog' },
-  { petName: 'Tweetie', petType: 'canary' },
-  { petName: 'Goldie', petType: 'fish' },
-]
+  { petName: "Fido", petType: "dog" },
+  { petName: "Tweetie", petType: "canary" },
+  { petName: "Goldie", petType: "fish" },
+];
+
+const initialFormValues = { petName: "", petType: "" };
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+  const submit = (evt) => {
+    evt.preventDefault();
+    const newPet = {
+      petName: formValues.petName.trim(),
+      petType: formValues.petType.trim(),
+    };
+    setPets(pets.concat(newPet));
+    setFormValues(initialFormValues);
+  };
+  
+  return (
+    <div className="container">
+      <h1>Simple Form App</h1>
+      {
+        
+        pets.map((pet, idx) => (
+          <div key={idx}>
+            {pet.petName} is a {pet.petType}
+          </div>
+        ))
+      }
+      <form onSubmit={submit}>
+        <input
+          value={formValues.petName}
+          onChange={change}
+          name="petName"
+          type="text"
+        />
+        <input
+          value={formValues.petType}
+          onChange={change}
+          name="petType"
+          type="text"
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
 
 render(
   <>
     <SimpleForm />
-    {/* <App /> */}
-  </>
-  , document.querySelector('#root')
-)
+    <App />
+  </>,
+  document.querySelector("#root")
+);
